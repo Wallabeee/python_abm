c5f0692c4d69e617beb25d43a2660d6eb14cf821
Wallabeee <47649764+Wallabeee@users.noreply.github.com>
2022-07-28 20:54:31 -0400

    Initial commit

---
+++ Intro_tutorial/sim.py
@@ -0,0 +1,17 @@
+from money_model import *
+import matplotlib.pyplot as plt
+
+all_wealth = []
+# This runs the model 100 times, each model executing 10 steps.
+for j in range(100):
+    # Run the model
+    model = MoneyModel(10)
+    for i in range(10):
+        model.step()
+
+    # Store the results
+    for agent in model.schedule.agents:
+        all_wealth.append(agent.wealth)
+
+plt.hist(all_wealth, bins=range(max(all_wealth) + 1))
+plt.show()
